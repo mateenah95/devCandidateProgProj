c739b5ea92a3c6d8d84dfc7847d51ea056294bf1
Mateen <mateenah95@gmail.com>
2019-06-04 14:12:48 -0400

    imported and set api key

--- application.py
+++ application.py
@@ -1,6 +1,7 @@
-import csv
+#importing the CSV module & easypost module
+import csv, easypost
 
-
+easypost.api_key = 'EZTKb4661e503603421d8dd125dc8e383aa4hY4mwPbdKTnhsCy2CwfUYA'
 
 with open('sample-data.csv', 'r') as csv_file:
     csv_data = csv.reader(csv_file)
